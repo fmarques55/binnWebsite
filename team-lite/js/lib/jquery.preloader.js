f2b38220123d52f744556d412fa5bf7b34a956d7
Felipe <felipe.sampaio@aeb.gov.br>
2016-03-01 17:35:34 -0300

    Removendo umas parada fudida que tinha subido nessa porra não sei como, acho que foi quando o lucas fuçou a porra do meu ubuntu e cagou essa merda, filho da puta

--- team-lite/js/lib/jquery.preloader.js
+++
@@ -1,9 +0,0 @@
-/* Preloader by Shahriar 
- * http://scripteden.com/ 
- **/
-(function ($) {
-    $(window).load(function () {
-        $('#status').fadeOut();
-        $('#preloader').delay(300).fadeOut('slow');
-    });
-}(jQuery));
